fd9153592102d180da3b6c59762f118d4f605fa2
Lane Campbell <ldc0618@gmail.com>
2025-06-11 16:11:13 -0700

    stripe update working

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,6 +1,7 @@
 import Stripe from "stripe";
-import { updateRecord } from "./data";
+import { getRecords, updateRecord } from "./data";
 import { getUserFromEmail } from "./auth";
+import { return200, return500 } from "./return-types";
 
 // Add simple log function if not available
 const log = (context, message) => {
@@ -20,6 +21,7 @@ const stripeWebHookPost = async (context) => {
     if (!signature) {
       return context.text("", 400);
     }
+    // Get raw request body as text
     const body = await context.request.text();
     const event = await stripe.webhooks.constructEventAsync(
       body,
@@ -91,11 +93,86 @@ const stripeWebHookPost = async (context) => {
         });
         break;
       }
+      case "customer.updated": {
+        const customer = event.data.object;
+        const email = customer.email;
+        // need to add stripe customer id to user profile
+        const d1 = context.locals.runtime.env.D1;
+        const user = await getUserFromEmail(d1, email);
+        const profile = JSON.parse(user.profile);
+        profile.stripeCustomerId = customer.id;
+        if (!user) {
+          log(context, {
+            message: `User not found for email: ${email}`,
+          });
+          return;
+        }
+        await updateRecord(
+          d1,
+          {},
+          {
+            id: user.id,
+            table: "users",
+            data: {
+              profile: JSON.stringify(profile),
+            },
+          },
+          {}
+        );
+        log(context, {
+          message: `Customer updated: ${customer.id}`,
+          email,
+        });
+        break;
+      }
       case "customer.subscription.created": {
         const subscription = event.data.object;
+        const plan = subscription.items.data[0].plan.interval;
+        const customerId = subscription.customer;
+
+        const userRecord = await getRecords(
+            context,
+            "users", // table name
+            {
+              filters: {
+                profile: {
+                  $contains: customerId, // the email address you want to look up
+                },
+              },
+            },
+            `user-lookup-${customerId}`, // cache key
+            "fastest"
+          );
+
+
+        if (!userRecord.data || !userRecord.data.length) {
+          log(context, {
+            message: `User not found for customer id: ${customerId}`,
+          });
+          return;
+        }
+
+        const user = userRecord.data[0];
+        console.log("user-->", user);
+
+        const profile = JSON.parse(user.profile);
+        profile.plan = plan;
+
+        await updateRecord(
+          context.locals.runtime.env.D1,
+          {},
+          {
+            id: user.id,
+            table: "users",
+            data: {
+              profile: JSON.stringify(profile),
+            },
+          },
+          {}
+        );
         log(context, {
           message: `New subscription created for customer: ${subscription.customer}`,
-          plan: subscription.items.data[0].plan.id,
+          plan: plan,
         });
         break;
       }
@@ -104,13 +181,13 @@ const stripeWebHookPost = async (context) => {
         break;
     }
 
-    return context.text("", 200);
+    return return200();
   } catch (err) {
     const errorMessage = `⚠️  Webhook signature verification failed. ${
       err instanceof Error ? err.message : "Internal server error"
     }`;
     console.error(errorMessage);
-    return context.text(errorMessage, 400);
+    return return500(errorMessage);
   }
 };
 
